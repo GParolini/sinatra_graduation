7f70ea485d56d1b6899fff429a8033f567c69257
Giuditta <giudittaparolini@gmail.com>
2019-01-22 20:12:40 +0100

    updated graduation file

--- graduation.rb
+++ graduation.rb
@@ -1,7 +1,8 @@
 require "sinatra"
+require "erb"
 
 get "/" do
-  "My Sinatra graduation project"
+  "<html><body><h1>My Sinatra graduation project</h1><h2>How to Build a Contact List Website</h2></body></html>"
 end
 
 get "/contacts" do
